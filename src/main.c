45a34f0c94f5be6ac49107fbcb278825843c3ad9
heolo1 <eldon15563+github@gmail.com>
2026-03-08 14:20:53 -0700

    add options, allowing for things like stdin reading

--- src/main.c
+++ src/main.c
@@ -1,11 +1,166 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "printing.h"
 
+bool arg_is_flag(const char *arg) {
+    // safe even for empty strings, since first condition will immediately fail
+    return arg[0] == '-' && arg[1] == '-';
+}
+
+typedef enum {
+    FLAG_V_no_label,
+    FLAG_V_label_0,
+    FLAG_V_label_1,
+} FLAG_VS_label;
+
+bool FLAG_stdin = false;
+bool FLAG_invalid = false;
+FLAG_VS_label FLAG_label = FLAG_V_no_label;
+
+// searches in [str + str_start, str + str_size) for \r, \n, or \0
+// returns str_size if not found
+int find_next_num_break(const char *str, int str_start, int str_size) {
+    int i;
+    for (i = str_start; i < str_size; i++) {
+        if (str[i] == '\0' || str[i] == '\n' || str[i] == '\r') {
+            break;
+        }
+    }
+    return i;
+}
+
 int main(int argc, char *argv[]) {
-    for (int i = 1; i < argc; i++) {
-        print_by_kind(argv[i], ss_get_num_kind(argv[i]));
-        printf("\n");
+    char **flagv = argv + 1;
+    int flagc = 0;
+    for (; flagc + 1 < argc && arg_is_flag(flagv[flagc]); flagc++);
+
+    for (int i = 0; i < flagc; i++) {
+        // i should replace this with something better sooner or later, probably
+        if (strcmp("--", flagv[i]) == 0 ||
+            strcmp("--stdin", flagv[i]) == 0) {
+            if (FLAG_stdin) {
+                printf("duplicate flag: %s\n", flagv[i]);
+                return 1;
+            } else {
+                FLAG_stdin = true;
+            }
+        } else if (strcmp("--!", flagv[i]) == 0 ||
+                   strcmp("--invalid", flagv[i]) == 0) {
+            if (FLAG_invalid) {
+                printf("duplicate flag: %s\n", flagv[i]);
+                return 1;
+            } else {
+                FLAG_invalid = true;
+            }
+        } else if (strcmp("--#", flagv[i]) == 0 ||
+                   strcmp("--labels", flagv[i]) == 0) {
+            if (FLAG_label != FLAG_V_no_label) {
+                printf("duplicate flag: %s\n", flagv[i]);
+                return 1;
+            } else {
+                FLAG_label = FLAG_V_label_1;
+            }
+        } else if (strcmp("--#0", flagv[i]) == 0 ||
+                   strcmp("--labels-0", flagv[i]) == 0) {
+            if (FLAG_label != FLAG_V_no_label) {
+                printf("duplicate flag: %s\n", flagv[i]);
+                return 1;
+            } else {
+                FLAG_label = FLAG_V_label_0;
+            }
+        } else {
+            printf("unrecognized flag: %s\n", flagv[i]);
+            return 1;
+        }
+    }
+
+    FILE *f_valid = stdout;
+    FILE *f_invalid = FLAG_invalid ? stderr : stdout;
+
+    int num_idx = FLAG_label == FLAG_V_label_1;
+
+    if (FLAG_stdin) {
+        // arbitrary constants
+        int read_size = 256;
+        int buf_size = read_size * 8;
+
+        int buf_pos = 0;
+        char *buf = malloc(sizeof(char) * buf_size);
+
+        int read_bytes;
+        // at the end of each loop, buf_pos is after the last read char
+        while ((read_bytes = read(STDIN_FILENO, buf + buf_pos, read_size)) > 0) {
+            // this is the end of our searching bytes, where buf_pos will go
+            int new_buf_pos = buf_pos + read_bytes;
+
+            // start looking for breaks
+            int break_pos = buf_pos;
+            buf_pos = 0;
+            // buf_pos from here acts as the beginning of a string, and break_pos as the exclusive end
+            while ((break_pos = find_next_num_break(buf, break_pos, new_buf_pos)) != new_buf_pos) {
+                // first need to make sure whether or not we should ignore this
+                int length = break_pos - buf_pos;
+                if (length) {
+                    ss_num_kind num_kind = ss_get_num_kindn(buf + buf_pos,
+                        length);
+                    FILE *f_out = get_desired_f(f_valid, f_invalid, num_kind);
+                    if (FLAG_label != FLAG_V_no_label) {
+                        fprintf(f_out, "%d: ", num_idx++);
+                    }
+                    print_by_kindn(f_valid, f_invalid, buf + buf_pos, length,
+                        num_kind);
+                    putc('\n', f_out);
+                }
+
+                // look past the delimiter at break_pos
+                buf_pos = ++break_pos;
+            }
+
+            // want to move data if any numbers were found and parsed
+            if (buf_pos) {
+                memmove(buf, buf + buf_pos, new_buf_pos -= buf_pos);
+            }
+
+            buf_pos = new_buf_pos;
+            // expand buf if necessary
+            if (buf_pos + read_size >= buf_size) {
+                buf_size *= 2;
+                char *new_buf = malloc(sizeof(char) * buf_size);
+                memcpy(new_buf, buf, buf_pos);
+                free(buf);
+                buf = new_buf;
+            }
+        }
+
+        // eof reached, check if there are any numbers remaining at the end
+        if (buf_pos) {
+            ss_num_kind num_kind = ss_get_num_kindn(buf, buf_pos);
+            FILE *f_out = get_desired_f(f_valid, f_invalid, num_kind);
+            if (FLAG_label != FLAG_V_no_label) {
+                fprintf(f_out, "%d: ", num_idx++);
+            }
+            print_by_kindn(f_valid, f_invalid, buf, buf_pos, num_kind);
+            putc('\n', f_out);
+        }
+
+        free(buf);
+    } else {
+        char **numv = flagv + flagc;
+        int numc = argc - flagc - 1;
+
+        for (int i = 0; i < numc; i++) {
+            ss_num_kind num_kind = ss_get_num_kind(numv[i]);
+            FILE *f_out = get_desired_f(f_valid, f_invalid, num_kind);
+            if (FLAG_label != FLAG_V_no_label) {
+                fprintf(f_out, "%d: ", num_idx++);
+            }
+            print_by_kind(f_valid, f_invalid, numv[i], num_kind);
+            putc('\n', f_out);
+        }
     }
 
     return 0;
